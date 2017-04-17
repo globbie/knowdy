7da59ee98ec1977459c480cc0cc97f863fbf137d
Dmitri Dmitriev <dmitri@globbie.net>
2017-04-17 16:07:52 +0300

    Legacy dict structure : to be replaced with kndRefSet

---
+++ src/ooconfig.h
@@ -0,0 +1,240 @@
+/**
+ *   Copyright (c) 2011-2012 by Dmitri Dmitriev
+ *   All rights reserved.
+ *
+ *   This file is part of the OOmnik Conceptual Processor, 
+ *   and as such it is subject to the license stated
+ *   in the LICENSE file which you have received 
+ *   as part of this distribution.
+ *
+ *   Project homepage:
+ *   <http://www.oomnik.ru>
+ *
+ *   Initial author and maintainer:
+ *         Dmitri Dmitriev aka M0nsteR <dmitri@globbie.net>
+ *
+ *   -----------
+ *   ooconfig.h
+ *   OOmnik configuration settings
+ */
+
+#ifndef OO_CONFIG_H
+#define OO_CONFIG_H
+
+/* converting enum names to strings */
+#define OO_STR(s) #s
+
+/* return error codes */
+enum { oo_OK, oo_FAIL, oo_NOMEM, oo_MATCH, oo_NO_RESULTS } oo_err_codes;
+
+static const char *oo_err_codes_text[] =  \
+  { "Everything is OK", 
+    "Some internal error occured", 
+    "Not enough memory :(",
+    "Match found!",
+    "No results :("  };
+
+#ifdef WIN32
+  #ifndef NO_BUILD_DLL
+      #define EXPORT __declspec(dllexport)
+  #else
+      #define EXPORT __declspec(dllimport)
+  #endif
+#else
+  #define EXPORT
+#endif
+
+#if defined(__STDC_VERSION__) && __STDC_VERSION__ >= 199901L
+
+#include <stdbool.h>
+
+#else
+
+#undef bool
+#undef true
+#undef false
+
+#define bool unsigned char
+#define true  1
+#define false 0
+
+#endif
+
+
+#define INTERACT_INPUT_BUF_SIZE 256
+
+#define INPUT_BUF_SIZE 256
+
+/*#define SEGM_SIZE 512*/
+
+#define ACCU_CONCFREQ_STORAGE_SIZE 512
+#define ACCU_MAX_CONCFREQS 10
+
+#define SOLVER_CODEUNIT_STORAGE_SIZE 512
+#define SOLVER_INDEX_SIZE 1024 * 4
+#define SOLVER_COMPLEX_STORAGE_SIZE 1024 * 10
+
+#define SOLVER_REJECT_STORAGE_SIZE SOLVER_COMPLEX_STORAGE_SIZE
+
+#define CODEUNIT_COMPLEX_POOL_SIZE 4
+
+#define INTERP_POOL_SIZE 8
+
+#define TOPIC_POOL_SIZE 12
+#define TOPIC_SHOW_LIMIT 3
+#define NUM_TOPIC_INGREDIENTS 32
+#define CONC_RATING_SIZE 32
+
+#define LINEAR_SOLUTION_STORAGE_SIZE 1024 * 1024
+#define MAX_LINEAR_SOLUTIONS 2
+
+#define DEFAULT_CONCEPT_COMPLEXITY 1.0
+
+
+#define SOLVER_COMPLEX_INDEX_SIZE \
+     SOLVER_CODEUNIT_STORAGE_SIZE
+
+#define NUM_GROUP_ITEMS 1
+
+#define OUTPUT_BUF_SIZE 1024 * 1024
+#define TEMP_BUF_SIZE 256
+
+#define INDEX_REALLOC_FACTOR 2
+#define DEFAULT_INDEX_SIZE 1024
+
+#define NUM_CACHE_TAILS 128
+
+/* hash table sizes */
+#define OO_HUGE_DICT_SIZE 100000
+#define OO_LARGE_DICT_SIZE 10000
+#define OO_MEDIUM_DICT_SIZE 1000
+#define OO_SMALL_DICT_SIZE 100
+#define OO_TINY_DICT_SIZE 10
+
+#define STORAGE_CACHE_SIZE 1024
+
+#define OO_MAZE_ITEM_STORAGE_SIZE 1024 * 1024
+#define OO_MAZE_SPEC_STORAGE_SIZE 1024 * 1024
+#define OO_MAZE_LOC_STORAGE_SIZE 1024 * 1024
+
+#define OO_MAX_MAZE_DEPTH 3
+
+/* debugging output levels */
+#define DEBUG_LEVEL_1 0
+#define DEBUG_LEVEL_2 0
+#define DEBUG_LEVEL_3 0
+#define DEBUG_LEVEL_4 0
+
+#define DEBUG_CS_LEVEL_1 0
+#define DEBUG_CS_LEVEL_2 0
+#define DEBUG_CS_LEVEL_3 0
+#define DEBUG_CS_LEVEL_4 0
+
+#define DEBUG_SEGM_LEVEL_1 0
+#define DEBUG_SEGM_LEVEL_2 0
+#define DEBUG_SEGM_LEVEL_3 0
+#define DEBUG_SEGM_LEVEL_4 0
+
+#define DEBUG_CACHE_LEVEL_1 0
+#define DEBUG_CACHE_LEVEL_2 0
+#define DEBUG_CACHE_LEVEL_3 0
+#define DEBUG_CACHE_LEVEL_4 0
+
+#define DEBUG_CONC_LEVEL_1 0
+#define DEBUG_CONC_LEVEL_2 0
+#define DEBUG_CONC_LEVEL_3 0
+#define DEBUG_CONC_LEVEL_4 0
+
+#define DEBUG_COMPLEX_LEVEL_1 1
+#define DEBUG_COMPLEX_LEVEL_2 1
+#define DEBUG_COMPLEX_LEVEL_3 1
+#define DEBUG_COMPLEX_LEVEL_4 1
+
+#define DEBUG_SOLVER_LEVEL_1 1
+#define DEBUG_SOLVER_LEVEL_2 1
+#define DEBUG_SOLVER_LEVEL_3 1
+#define DEBUG_SOLVER_LEVEL_4 1
+
+#define NUM_TEST_RUNS 1
+
+/* grades from 0 to 1:  total 256 grades */
+#define grade_t unsigned char
+
+/* max number of atrributes in a Concept */
+#define attr_size_t unsigned short
+#define ATTR_MAX (unsigned short)-1
+
+#define NAME_ATOMS_MAX 255
+
+/* max number of offsets 
+ * in string representation of concepts */
+#define STR_DEPTH_LIMIT 12
+
+/* num of whitespace chars */
+#define OFFSET_SIZE 2
+#define SPACE_CHAR 32
+
+/* max number of concepts in the MindMap */
+#define mindmap_size_t size_t /*unsigned long*/
+
+
+#define ooATOM const unsigned char
+#define MAX_UNREC_ATOMS 32
+
+#define OO_MAX_GREED_LIMIT 4
+#define COMPLEX_MAX_DISTANCE 4
+
+/* penalty for each missing unit in coverage */
+#define COMPLEX_PENALTY 10
+
+/* weight gain bonus for correct linking */
+#define OPER_SUCCESS_BONUS 20
+#define ROLEMARKER_BONUS 10
+
+#define PRIMARY_USAGE_WEIGHT_BONUS 2
+
+/* RAG binary packing: size in bytes */
+#define CONCID_BIN_SIZE 8
+#define ATTR_BIN_SIZE 1
+#define POS_BIN_SIZE 4
+#define CODEUNIT_TYPE_SIZE 2
+
+/* syntax */
+#define MAX_PARENTS 16
+
+#define MAX_CONTACT_DISTANCE 0
+
+#define ATOM_BYTE 1
+
+#define MAX_CONC_ID_SIZE 256
+
+#define MAX_DOMAIN_TITLE_LENGTH 3
+#define CONC_BATCH_SIZE 200
+
+/* caching in bytes */
+#define MAX_MEMCACHE_SIZE 900 * 1024 * 1024
+#define DEFAULT_MATRIX_DEPTH 3
+#define DEFAULT_MAX_UNREC_CHARS 10
+
+#define UCS2_MAX 65535
+#define NUMERIC_CODE_TYPE size_t
+
+#define CODE_VERIFICATION_BONUS 10
+
+typedef enum pack_type { PACK_COMPACT, 
+                         PACK_RAG,
+                         PACK_ROBUST,
+			 PACK_SECURE,
+			 PACK_XML
+                       } pack_type;
+
+typedef enum output_type {  OO_FORMAT_GSL,
+			    OO_FORMAT_JSON, 
+			    OO_FORMAT_XML
+} output_type;
+
+
+typedef int (*oo_compar_func)(const void *item, 
+			      const void *another_item);
+
+#endif
---
+++ src/oodict.c
@@ -0,0 +1,402 @@
+/**
+ *   Copyright (c) 2011-2013 by Dmitri Dmitriev
+ *   All rights reserved.
+ *
+ *   This file is part of the OOmnik Conceptual Processor, 
+ *   and as such it is subject to the license stated
+ *   in the LICENSE file which you have received 
+ *   as part of this distribution.
+ *
+ *   Project homepage:
+ *   <http://www.oomnik.ru>
+ *
+ *   Initial author and maintainer:
+ *         Dmitri Dmitriev aka M0nsteR <dmitri@globbie.net>
+ *
+ *   Code contributors:
+ *         * Jenia Krylov <info@e-krylov.ru>
+ *
+ *   --------
+ *   oodict.c
+ *   OOmnik Dictionary implementation
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "oodict.h"
+#include "oolist.h"
+
+static size_t 
+oo_hash(const char *key)
+{
+    const char *p = key;
+    size_t h = 0;
+
+    if (!key) return 0;
+
+    while (*p)
+        h = (h << 1) ^ *p++;
+
+    return h;
+}
+
+static const char*
+ooDict_str(struct ooDict *self)
+{
+    struct ooListItem *cur;
+    struct ooDictItem *item;
+    struct ooList *l;
+    int i;
+
+    printf("  dict size: %lu\n",
+           (unsigned long)self->hash->size);
+
+    for (i = 0; i < self->hash->size; i++) {
+        l = (struct ooList*)self->hash->data[i];
+        if (l->size == 0) continue;
+	cur = l->head;
+
+	while (cur) {
+	    item = (struct ooDictItem*)cur->data;
+
+	    printf(" %d) KEY: \"%s\"\n", i, item->key);
+
+	    cur = cur->next;
+        }
+    }
+
+    printf("TOTAL KEYS: %lu\n", (unsigned long)self->size);
+
+    return "";
+}
+
+static int
+ooDict_set_hash(struct ooDict *self,
+		oo_hash_func new_hash)
+{
+    if (!new_hash) return oo_FAIL;
+    self->hash_func = new_hash;
+
+    return oo_OK;
+}
+
+static ooDictItem* 
+ooDict_find_item(struct ooDict *self,
+		 const char *key)
+{
+    struct ooList *l;
+    struct ooListItem *cur;
+    const char *cur_key;
+    unsigned int h;
+
+    h = oo_hash(key) % self->hash->size;
+
+    l = (struct ooList*)self->hash->data[h];
+
+    cur = l->head;
+    while (cur) {
+        cur_key = ((struct ooDictItem*)cur->data)->key;
+        if (!strcmp(key, cur_key))
+            return (struct ooDictItem*)cur->data;
+        cur = cur->next;
+    }
+
+    return NULL;
+}
+
+static void*
+ooDict_get(struct ooDict *self,
+	   const char *key)
+{
+    struct ooDictItem *item;
+    if (!key) return NULL;
+
+    item = ooDict_find_item(self, key);
+    if (!item) return NULL;
+
+    return item->data;
+}
+
+static int
+ooDict_set(struct ooDict *self,
+	   const char *key,
+	   void *data)
+{
+    struct ooList *l;
+    struct ooListItem *cur;
+    struct ooDictItem *item = NULL;
+    const char *cur_key;
+    unsigned int h;
+
+    if (!key) return oo_FAIL;
+
+    h = oo_hash(key) % self->hash->size;
+
+    l = (struct ooList*)self->hash->data[h];
+
+    cur = l->head;
+
+    while (cur) {
+        cur_key = ((struct ooDictItem*)cur->data)->key;
+        if (!strcmp(key, cur_key)) {
+            item = (struct ooDictItem*)cur->data;
+            break;
+        }
+        cur = cur->next;
+    }
+
+    if (item) {
+        item->data = data;
+        return oo_OK;
+    }
+
+    item = (struct ooDictItem*)malloc(sizeof(struct ooDictItem));
+    if (!item) return oo_NOMEM;
+
+    item->data = data;
+    item->key = strdup(key);
+
+    l->add(l, (void*)item, NULL);
+    self->size++;
+
+    return oo_OK;
+}
+
+static bool 
+ooDict_key_exists(struct ooDict *self,
+		  const char *key)
+{
+    struct ooDictItem *i = ooDict_find_item(self, key);
+    return (i != NULL);
+}
+
+static int
+ooDict_remove(struct ooDict *self,
+	      const char *key)
+{
+    struct ooList *l;
+    char *cur_key;
+    void *data;
+    struct ooListItem *cur;
+    unsigned int h;
+    
+    h = self->hash_func(key) % self->hash->size;
+    l = (ooList*)self->hash->data[h];
+
+    cur = l->head;
+    while (cur) {
+        cur_key = ((struct ooDictItem*)cur->data)->key;
+
+        if (!strcmp(key, cur_key)) {
+            data = ((struct ooDictItem*)cur->data)->data;
+            free(cur_key);
+
+            cur->data = NULL;
+            l->remove(l, cur);
+	    self->size--;
+            return oo_OK;
+        }
+        cur = cur->next;
+    }
+
+    return oo_FAIL;
+}
+
+static int ooDict_del(struct ooDict *self)
+{
+    unsigned int i;
+    struct ooList *l;
+    struct ooListItem *cur;
+    struct ooDictItem *item;
+
+    for (i = 0; i < self->hash->size; ++i) {
+        l = (struct ooList*)self->hash->data[i];
+        if (l->size) {
+            cur = l->head;
+            while (cur) {
+                item = (struct ooDictItem*)cur->data;
+                free(item->key);
+                free(item);
+                cur = cur->next;
+            }
+        }
+        l->del(l);
+    }
+
+    self->hash->del(self->hash);
+
+    free(self);
+
+    return oo_OK;
+}
+
+static int
+ooDict_resize(struct ooDict *self,
+	      unsigned int new_size)
+{
+    unsigned int i;
+    struct ooList *l;
+    struct ooListItem *cur;
+    struct ooDictItem *item;
+    struct ooArray *tmp;
+    size_t h;
+    struct ooList *new_list;
+    int ret;
+
+    tmp = self->hash->get_subsequence(self->hash, 0, 
+				      self->hash->size);
+    ret = self->hash->resize(self->hash, new_size);
+
+    for (i = 0; i < new_size; ++i) {
+        ret = ooList_new(&l);
+	if (ret != oo_OK) return ret;
+
+        self->hash->data[i] = (void*)l;
+    }
+
+    /* rehash all items */
+    for (i = 0; i < tmp->size; ++i) {
+        l = (ooList*)tmp->data[i];
+        if (l->size == 0) continue; 
+	cur = l->head;
+	while (cur) {
+	    item = (struct ooDictItem*)cur->data;
+	    h = self->hash_func(item->key) % self->hash->size;
+	    new_list = (struct ooList*)self->hash->data[h];
+	    new_list->add(new_list, (void*)item, NULL);
+	    cur = cur->next;
+        }
+        l->del(l);
+    }
+    tmp->del(tmp);
+    return oo_OK;
+}
+
+static int 
+ooDict_rewind(struct ooDict *self)
+{
+    self->curr_pos = 0;
+    self->curr_item = NULL;
+    return oo_OK;
+}
+
+static int 
+ooDict_next_item(struct ooDict *self,
+		 const char **key,
+		 void **data)
+{
+    struct ooList *l;
+    struct ooListItem *item = self->curr_item;
+    struct ooDictItem *item_data = NULL;
+    size_t i;
+
+    /* if curr_item is not NULL, continue listing */
+
+    if (item) {
+	item_data = (struct ooDictItem*)item->data;
+	*key = item_data->key;
+	*data = item_data->data;
+
+	self->curr_item = item->next;
+	return oo_OK;
+    }
+
+    /* jump to the next position in hash */
+
+    if (self->curr_pos >= self->hash->size)
+	goto no_results;
+    
+    for (i = self->curr_pos; i <  self->hash->size; i++) {
+	l = (struct ooList*)self->hash->data[i];
+	if (l->size == 0) continue;
+	item = l->head;
+	break;
+    }
+
+    if (!item)
+	goto no_results;
+    
+    item_data = (struct ooDictItem*)item->data;
+    *key = item_data->key;
+    *data = item_data->data;
+
+    self->curr_item = item->next;
+    self->curr_pos = i + 1;
+
+    return oo_OK;
+
+no_results:
+    *key = NULL; 
+    *data = NULL;
+    return oo_FAIL;
+}
+
+
+
+static int 
+ooDict_init(struct ooDict *self)
+{
+    self->size          = 0;
+    self->del           = ooDict_del;
+    self->str           = ooDict_str;
+    self->remove        = ooDict_remove;
+    self->get           = ooDict_get;
+    self->set           = ooDict_set;
+    self->key_exists    = ooDict_key_exists;
+    self->resize        = ooDict_resize;
+    self->rewind        = ooDict_rewind;
+    self->next_item     = ooDict_next_item;
+    self->set_hash      = ooDict_set_hash;
+    self->hash_func     = oo_hash;
+
+    self->curr_item = NULL;
+    self->curr_pos = 0;
+
+    return oo_OK;
+}
+
+/* constructor */
+extern int
+ooDict_new(struct ooDict **dict, 
+	   size_t init_size)
+{
+    size_t i;
+    struct ooList *l;
+    int ret;
+
+    struct ooDict *self = malloc(sizeof(struct ooDict));
+    if (!self) return oo_NOMEM;
+
+    if (init_size == 0) return oo_FAIL;
+
+    ooDict_init(self);
+
+    ooArray_new(&self->hash);
+    if (!self->hash) {
+	self->del(self);
+	return oo_NOMEM;
+    }
+
+    ret = self->hash->resize(self->hash, init_size);
+    if (ret != oo_OK) {
+	self->del(self);
+	return ret;
+    }
+
+    for (i = 0; i < init_size; ++i) {
+        ret = ooList_new(&l);
+	if (ret != oo_OK) {
+	    self->del(self);
+	    return ret;
+	}
+
+        self->hash->data[i] = l;
+    }
+
+    *dict = self;
+
+    return oo_OK;
+}
