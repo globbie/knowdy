49d0f65974b8d8c76e2b483a14bbba497a622d2b
Dmitri Dmitriev <dmitri@globbie.net>
2017-08-29 11:48:08 +0300

    attr num type

--- core/src/knd_concept.c
+++ core/src/knd_concept.c
@@ -608,6 +608,35 @@ static int parse_str_change(void *obj,
     return knd_OK;
 }
 
+static int parse_num_change(void *obj,
+                            const char *rec,
+                            size_t *total_size)
+{
+    struct kndConcept *self = (struct kndConcept*)obj;
+    struct kndAttr *attr;
+    int err;
+
+    err = kndAttr_new(&attr);
+    if (err) return err;
+    attr->parent_conc = self;
+    attr->type = KND_ATTR_NUM;
+
+    err = attr->parse(attr, rec, total_size);
+    if (err) {
+        knd_log("-- failed to parse the NUM attr: %d", err);
+        return err;
+    }
+    if (!self->tail_attr) {
+        self->tail_attr = attr;
+        self->attrs = attr;
+    }
+    else {
+        self->tail_attr->next = attr;
+        self->tail_attr = attr;
+    }
+    return knd_OK;
+}
+
 static int parse_ref_change(void *obj,
                             const char *rec,
                             size_t *total_size)
@@ -997,6 +1026,12 @@ static int parse_import_class(void *obj,
           .name = "str",
           .name_size = strlen("str"),
           .parse = parse_str_change,
+          .obj = c
+        },
+        { .type = KND_CHANGE_STATE,
+          .name = "num",
+          .name_size = strlen("num"),
+          .parse = parse_num_change,
           .obj = c
         },
         {  .type = KND_CHANGE_STATE,
