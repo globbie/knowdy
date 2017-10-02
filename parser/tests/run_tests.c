3add548307ea5293f6cbda9b9d74d9fbdf14d3cd
Ilia K <ki.stfu@gmail.com>
2017-10-02 19:02:32 +0300

    parser/tests: Add tests for implied field

--- parser/tests/run_tests.c
+++ parser/tests/run_tests.c
@@ -10,28 +10,94 @@
 int rc;
 const char *rec;
 size_t total_size;
-struct { char sid[6]; size_t sid_size; } user;
+struct { char name[KND_SHORT_NAME_SIZE]; size_t name_size; char sid[6]; size_t sid_size; } user;
 
-void setup(void) {
+
+// Common routines
+static void setup(void) {
+    user.name_size = 0;
     user.sid_size = 0;
 }
 
+static int run_set_name(void *obj, struct kndTaskArg *args, size_t num_args) {
+    ck_assert(&user == obj);
+    ck_assert(args && num_args == 1);
+    ck_assert(args[0].name_size == strlen("_impl") && !memcmp(args[0].name, "_impl", args[0].name_size));
+    ck_assert(args[0].val_size != 0);
+    if (args[0].val_size > sizeof user.name)
+        return knd_LIMIT;
+    memcpy(user.name, args[0].val, args[0].val_size);
+    user.name_size = args[0].val_size;
+    return knd_OK;
+}
+
+//struct kndTaskSpec
+//    name_spec = { .is_implied = true, .run = NULL, .obj = &user },
+//    sid_spec = { .name = "sid", .name_size = sizeof "sid" - 1, .buf = user.sid, .buf_size = &user.sid_size, .max_buf_size = sizeof user.sid };
+
+
 START_TEST(parse_task_empty)
-    struct kndTaskSpec specs[] = {{ .name = "sid", .name_size = strlen("sid"), .buf = user.sid, .buf_size = &user.sid_size, .max_buf_size = sizeof user.sid }};
+    struct kndTaskSpec specs[] = {
+        { .is_implied = true, .run = run_set_name, .obj = &user },
+        { .name = "sid", .name_size = strlen("sid"), .buf = user.sid, .buf_size = &user.sid_size, .max_buf_size = sizeof user.sid }
+    };
 
     rc = knd_parse_task(rec = "", &total_size, specs, sizeof specs / sizeof specs[0]);
     ck_assert(rc == knd_OK);
     ck_assert(total_size == strlen(rec));
-    ck_assert(user.sid_size == 0);
+    ck_assert(user.name_size == 0 && user.sid_size == 0);
 END_TEST
 
 START_TEST(parse_task_empty_with_spaces)
-    struct kndTaskSpec specs[] = {{ .name = "sid", .name_size = strlen("sid"), .buf = user.sid, .buf_size = &user.sid_size, .max_buf_size = sizeof user.sid }};
+    struct kndTaskSpec specs[] = {
+        { .is_implied = true, .run = run_set_name, .obj = &user },
+        { .name = "sid", .name_size = strlen("sid"), .buf = user.sid, .buf_size = &user.sid_size, .max_buf_size = sizeof user.sid }
+    };
 
     rc = knd_parse_task(rec = "     ", &total_size, specs, sizeof specs / sizeof specs[0]);
     ck_assert(rc == knd_OK);
     ck_assert(total_size == strlen(rec));
-    ck_assert(user.sid_size == 0);
+    ck_assert(user.name_size == 0 && user.sid_size == 0);
+END_TEST
+
+START_TEST(parse_implied_field_with_spaces)
+    struct kndTaskSpec specs[] = {{ .is_implied = true, .run = run_set_name, .obj = &user }};
+
+    rc = knd_parse_task(rec = " John Smith", &total_size, specs, sizeof specs / sizeof specs[0]);
+    ck_assert(rc == knd_OK);
+    ck_assert(total_size == strlen(rec));
+    ck_assert(user.name_size == strlen("John Smith") && !memcmp(user.name, "John Smith", user.name_size));
+
+    rc = knd_parse_task(rec = " John Space ", &total_size, specs, sizeof specs / sizeof specs[0]);
+    ck_assert(rc == knd_OK);
+    ck_assert(total_size == strlen(rec));
+    ck_assert(user.name_size == strlen("John Space ") && !memcmp(user.name, "John Space ", user.name_size));
+END_TEST
+
+START_TEST(parse_implied_field_max_size)
+    struct kndTaskSpec specs[] = {{ .is_implied = true, .run = run_set_name, .obj = &user }};
+    const char buf[KND_SHORT_NAME_SIZE + 1] = { [0 ... KND_SHORT_NAME_SIZE - 1] = 'a', [KND_SHORT_NAME_SIZE] = '\0' };
+
+    rc = knd_parse_task(rec = buf, &total_size, specs, sizeof specs / sizeof specs[0]);
+    ck_assert(rc == knd_OK);
+    ck_assert(total_size == strlen(rec));
+    ck_assert(user.name_size == strlen(buf) && !memcmp(user.name, buf, user.name_size));
+END_TEST
+
+START_TEST(parse_implied_field_max_size_plus_one)
+    struct kndTaskSpec specs[] = {{ .is_implied = true, .run = run_set_name, .obj = &user }};
+    const char buf[KND_SHORT_NAME_SIZE + 2] = { [0 ... KND_SHORT_NAME_SIZE] = 'a', [KND_SHORT_NAME_SIZE + 1] = '\0' };
+
+    rc = knd_parse_task(rec = buf, &total_size, specs, sizeof specs / sizeof specs[0]);
+    ck_assert(rc == knd_LIMIT);  // defined in run_set_name()
+END_TEST
+
+START_TEST(parse_implied_field_size_NAME_SIZE_plus_one)
+    struct kndTaskSpec specs[] = {{ .is_implied = true, .run = run_set_name, .obj = &user }};
+    const char buf[KND_NAME_SIZE + 2] = { [0 ... KND_NAME_SIZE] = 'a', [KND_NAME_SIZE + 1] = '\0' };
+
+    rc = knd_parse_task(rec = buf, &total_size, specs, sizeof specs / sizeof specs[0]);
+    ck_assert(rc == knd_LIMIT);
 END_TEST
 
 START_TEST(parse_tag_empty)
@@ -102,6 +168,10 @@ int main() {
    tcase_add_checked_fixture(tc, setup, NULL);
    tcase_add_test(tc, parse_task_empty);
    tcase_add_test(tc, parse_task_empty_with_spaces);
+   tcase_add_test(tc, parse_implied_field_with_spaces);
+   tcase_add_test(tc, parse_implied_field_max_size);
+   tcase_add_test(tc, parse_implied_field_max_size_plus_one);
+   tcase_add_test(tc, parse_implied_field_size_NAME_SIZE_plus_one);
    tcase_add_test(tc, parse_tag_empty);
    tcase_add_test(tc, parse_tag_empty_with_spaces);
    tcase_add_test(tc, parse_tag_unknown);
