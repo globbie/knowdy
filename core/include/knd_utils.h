f42265d64ae9a8b9a021eb2cc97b8a29ced5f347
Dmitri Dmitriev <dmitri@globbie.net>
2018-10-04 15:15:44 +0300

    remove the fossils from knd_utils

--- core/include/knd_utils.h
+++ core/include/knd_utils.h
@@ -7,24 +7,6 @@
 
 #include "knd_config.h"
 
-//typedef enum output_dest_t { KND_SEARCH_RESULTS,
-//			     KND_IDX,
-//			     KND_OBJ_META,
-//                             KND_OBJ_REPR,
-//			     KND_USER_META,
-//                             KND_USER_REPR,
-//                             KND_DBREC,
-//                             KND_ERR_MSG,
-//                             KND_UPDATE,
-//			     KND_TOPICS,
-//			     KND_SPECS  } output_dest_t;
-
-
-//extern int knd_compare(const char *a, const char *b);
-//extern int knd_inc_id(char *id);
-//extern int knd_is_valid_id(const char *id, size_t id_size);
-//extern int knd_state_is_valid(const char *id, size_t id_size);
-//extern int knd_next_state(char *s);
 extern void  knd_calc_num_id(const char *id, size_t id_size, size_t *numval);
 extern void knd_num_to_str(size_t numval, char *buf, size_t *buf_size, size_t base);
 
@@ -40,17 +22,13 @@ static inline void knd_uid_create(size_t seed, char *out_uid, size_t *out_uid_si
     knd_num_to_str(seed, out_uid, out_uid_size, KND_RADIX_BASE);
 }
 
-//extern const char *max_id(const char *a, const char *b);
-//extern const char *min_id(const char *a, const char *b);
-
 extern int knd_mkpath(const char *path, size_t path_size, mode_t mode, bool has_filename);
 
 extern int knd_write_file(const char *filename, 
                           void *buf, size_t buf_size);
 
-extern int 
-knd_append_file(const char *filename, 
-                const void *buf, size_t buf_size);
+extern int  knd_append_file(const char *filename, 
+                            const void *buf, size_t buf_size);
 
 extern int knd_make_id_path(char *buf,
 		     const char *path,
@@ -59,10 +37,6 @@ extern int knd_make_id_path(char *buf,
 
 extern int  knd_get_elem_suffix(const char *name,
                                 char *buf);
-//extern unsigned char * knd_pack_int(unsigned char *buf,
-//                                    unsigned int val);
-//extern unsigned long knd_unpack_int(const unsigned char *buf);
-
 extern void knd_remove_nonprintables(char *data);
 
 extern void knd_log(const char *fmt, ...);
