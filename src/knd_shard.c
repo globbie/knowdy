9d2e01bc3a5319cd9560184001e32d4007287730
Ilia K <ki.stfu@gmail.com>
2018-11-10 00:18:02 +0300

    task: kndTask's API accepts kndShard; Remove kndTask.root_repo; knd_task.select uses LocalContext

--- src/knd_shard.c
+++ src/knd_shard.c
@@ -58,7 +58,7 @@ extern int kndShard_run_task(struct kndShard *self,
         return err;
     }
 
-    err = kndTask_run(task, rec_start, rec_size);
+    err = kndTask_run(task, rec_start, rec_size, self);
     if (err != knd_OK) {
         task->error = err;
         knd_log("-- task running failure: %d", err);
@@ -201,7 +201,6 @@ int kndShard_new(struct kndShard **shard, const char *config, size_t config_size
 
     for (size_t i = 0; i < self->num_workers; i++) {
         task = self->workers[i];
-        task->root_repo = repo;
         task->user = user;
     }
 
