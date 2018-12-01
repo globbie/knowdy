bd192696bd38787afc87bae9aec9bdf74640c933
Ilia K <ki.stfu@gmail.com>
2018-12-02 00:18:09 +0300

    class select: Remove unused rels_presentation

--- src/knd_task.c
+++ src/knd_task.c
@@ -65,7 +65,6 @@ void kndTask_reset(struct kndTask *self)
     self->state_lt = 0;
     self->state_lte = 0;
     self->show_removed_objs = false;
-    self->show_rels = false;
     self->depth = 0;
     self->max_depth = 1;
 
