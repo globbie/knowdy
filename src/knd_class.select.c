2490b053b76502c09c816f8dba20708940b26186
Ilia K <ki.stfu@gmail.com>
2018-12-05 00:21:48 +0300

    class select: Clean up headers

--- src/knd_class.select.c
+++ src/knd_class.select.c
@@ -1,39 +1,16 @@
-#include <assert.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
-#include <time.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <sys/types.h>
-
-/* numeric conversion by strtol */
-#include <errno.h>
-#include <limits.h>
-
-#include "knd_config.h"
-#include "knd_mempool.h"
-#include "knd_repo.h"
-#include "knd_state.h"
 #include "knd_class.h"
-#include "knd_class_inst.h"
+
 #include "knd_attr.h"
 #include "knd_task.h"
-#include "knd_user.h"
-#include "knd_text.h"
-#include "knd_rel.h"
-#include "knd_proc.h"
-#include "knd_proc_arg.h"
+#include "knd_repo.h"
 #include "knd_set.h"
-#include "knd_utils.h"
-#include "knd_http_codes.h"
 
 #include <gsl-parser.h>
 #include <glb-lib/output.h>
 
-//#include <stddef.h>
+#include <assert.h>
+#include <stddef.h>
+#include <string.h>
 
 #define DEBUG_CLASS_SELECT_LEVEL_1 0
 #define DEBUG_CLASS_SELECT_LEVEL_2 0
