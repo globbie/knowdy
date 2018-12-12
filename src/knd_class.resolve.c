50f444dac01e5c4aeb902481985f9a00de63c11a
Dmitri Dmitriev <dmitri@globbie.net>
2018-12-12 15:12:21 +0300

    silence indexing debug

--- src/knd_class.resolve.c
+++ src/knd_class.resolve.c
@@ -69,19 +69,17 @@ static int inherit_attr(void *obj,
     err = attr_idx->add(attr_idx,
                         attr->id, attr->id_size,
                         (void*)ref);                                              RET_ERR();
-
     if (ref->attr_var) {
         if (attr->is_indexed) {
-            if (DEBUG_CLASS_RESOLVE_LEVEL_TMP) 
+            if (DEBUG_CLASS_RESOLVE_LEVEL_2) 
                 knd_log("..  indexing \"%.*s\" attr var in %.*s..",
                         attr->name_size, attr->name,
                         self->name_size, self->name);
             if (attr->is_a_set) {
-                err = knd_index_attr_var_list(self, attr, ref->attr_var, task);  RET_ERR();
+                err = knd_index_attr_var_list(self, attr, ref->attr_var, task);   RET_ERR();
             }
         }
     }
-
     return knd_OK;
 }
 
@@ -378,19 +376,17 @@ extern int knd_class_resolve(struct kndClass *self,
         err = link_baseclass(self, repo->root_class, task);                       RET_ERR();
     } else {
         err = resolve_baseclasses(self, task);                                    RET_ERR();
-
         for (cvar = self->baseclass_vars; cvar; cvar = cvar->next) {
             if (cvar->attrs) {
                 err = knd_resolve_attr_vars(self, cvar, task);                    RET_ERR();
             }
         }
     }
-
     /* primary attrs */
     if (self->num_attrs) {
         err = knd_resolve_primary_attrs(self, task);                              RET_ERR();
     }
-    
+
     if (DEBUG_CLASS_RESOLVE_LEVEL_2)
         knd_log("++ class \"%.*s\" id:%.*s resolved!",
             self->entry->name_size, self->entry->name,
